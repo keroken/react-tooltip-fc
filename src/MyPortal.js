23a4f4479cf60c6880568c030e9371fc02095b59
keroken <kenichi.shinagawa@gmail.com>
2021-08-04 00:30:41 +0900

    update MyPortal

--- src/MyPortal.js
+++ src/MyPortal.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
-export const MyPortal = (children) => {
+export const MyPortal = ({children}) => {
   const el = document.createElement('div');
 
-  React.useEffect(() => {
+  useEffect(() => {
     document.body.appendChild(el);
-    return () => {
-      document.body.removeChild(el);
-    };
+    return () => document.body.removeChild(el);
   }, [el]);
 
-  return ReactDOM.createPortal(children, el)
+  return createPortal(children, el)
 };
